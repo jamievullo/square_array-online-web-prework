0105480b4e444d503929592cffe6e15eaee522f2
Jamie Vullo <jvullo@protonmail.com>
2019-05-05 00:34:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
     new_array = []
-    array.each  {|element| new_array.push (element ** 2)}
+    array.each {|element| new_array.push (element ** 2)}
   
     return new_array
 
